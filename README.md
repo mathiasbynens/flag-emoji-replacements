3444c5bdb8eedc2f0b26d6496e166f016a7e3ab9
Mathias Bynens <mathias@qiwi.be>
2021-12-16 11:37:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Interesting flag emoji replacements
+
+[Ben Joeng shared an interesting observation](https://twitter.com/iwsfutcmd/status/1471250563677388800):
+
+```js
+'🇧🇬🇭🇷'.replace('🇬🇭', '🇦🇬'); // → '🇧🇦🇬🇷'
+```
+
+This repository contains every possible variation of this gotcha according to Unicode 14’s `RGI_Emoji_Flag_Sequence`. There’s [9,211,570 different variations clocking in at 718.50 MB](https://github.com/mathiasbynens/flag-emoji-replacements/tree/main/output), so the output is split across multiple files.
+
+Examples:
+
+```js
+'🇻🇦🇩🇲'.replace('🇦🇩', '🇬🇺'); // → '🇻🇬🇺🇲'
+'🇰🇷🇪🇹'.replace('🇷🇪', '🇳🇱'); // → '🇰🇳🇱🇹'
+'🇨🇵🇸🇨'.replace('🇵🇸', '🇻🇨'); // → '🇨🇻🇨🇨'
+'🇧🇧🇸🇱'.replace('🇧🇸', '🇪🇬'); // → '🇧🇪🇬🇱'
+'🇦🇹🇨🇺'.replace('🇹🇨', '🇸🇲'); // → '🇦🇸🇲🇺'
+'🇬🇱🇦🇪'.replace('🇱🇦', '🇮🇪'); // → '🇬🇮🇪🇪'
+'🇱🇸🇲🇭'.replace('🇸🇲', '🇰🇪'); // → '🇱🇰🇪🇭'
+'🇹🇻🇦🇫'.replace('🇻🇦', '🇨🇬'); // → '🇹🇨🇬🇫'
+'🇬🇭🇷🇺'.replace('🇭🇷', '🇸🇭'); // → '🇬🇸🇭🇺'
+'🇦🇬🇫🇮'.replace('🇬🇫', '🇱🇸'); // → '🇦🇱🇸🇮'
+'🇬🇸🇮🇹'.replace('🇸🇮', '🇹🇱'); // → '🇬🇹🇱🇹'
+'🇬🇮🇸🇬'.replace('🇮🇸', '🇧🇧'); // → '🇬🇧🇧🇬'
+'🇹🇹🇲🇦'.replace('🇹🇲', '🇬🇹'); // → '🇹🇬🇹🇦'
+'🇬🇼🇫🇲'.replace('🇼🇫', '🇧🇯'); // → '🇬🇧🇯🇲'
+'🇨🇰🇬🇦'.replace('🇰🇬', '🇲🇨'); // → '🇨🇲🇨🇦'
+'🇲🇳🇦🇴'.replace('🇳🇦', '🇫🇮'); // → '🇲🇫🇮🇴'
+'🇪🇦🇮🇨'.replace('🇦🇮', '🇭🇲'); // → '🇪🇭🇲🇨'
+'🇮🇹🇹🇭'.replace('🇹🇹', '🇳🇪'); // → '🇮🇳🇪🇭'
+'🇵🇬🇺🇬'.replace('🇬🇺', '🇲🇬'); // → '🇵🇲🇬🇬'
+'🇱🇸🇸🇽'.replace('🇸🇸', '🇰🇲'); // → '🇱🇰🇲🇽'
+'🇺🇾🇹🇻'.replace('🇾🇹', '🇲🇲'); // → '🇺🇲🇲🇻'
+'🇸🇪🇹🇩'.replace('🇪🇹', '🇱🇨'); // → '🇸🇱🇨🇩'
+'🇬🇪🇸🇭'.replace('🇪🇸', '🇸🇹'); // → '🇬🇸🇹🇭'
+'🇸🇩🇲🇬'.replace('🇩🇲', '🇬🇪'); // → '🇸🇬🇪🇬'
+'🇸🇲🇦🇮'.replace('🇲🇦', '🇧🇳'); // → '🇸🇧🇳🇮'
+```
+
+To get a random example:
+
+```sh
+shuf -n 1 < output/$(ls output/ | shuf -n 1)
+```
+
+The general pattern is the following, where `A`, `B`, `C`, `D`, `E`, and `F` are all flag emoji, and `{A, B}` are distinct from `{C, D, E, F}`:
+
+```js
+'AB'.replace('C', 'D'); // → 'EF'
+```
+
+Note that `{C, D}` may overlap with `{E, F}`, as that leads to even more confusing results, e.g.:
+
+```js
+'🇲🇦🇸🇱'.replace('🇦🇸', '🇲🇲'); // → '🇲🇲🇲🇱'
+```
+
+## Explanation
+
+Unicode defines the following 26 `Regional_Indicator` symbols:
+
+```
+🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿
+```
+
+Combining two of these into a [country code](https://unicode.org/reports/tr51/#Flags "Unicode region subtag") results in the flag emoji for that country. So, Ben’s example:
+
+```js
+'🇧🇬🇭🇷'.replace('🇬🇭', '🇦🇬'); // → '🇧🇦🇬🇷'
+```
+
+…is actually the following (with spaces added for clarity):
+
+```js
+'🇧 🇬 🇭 🇷'.replace('🇬 🇭', '🇦 🇬'); // → '🇧 🇦 🇬 🇷'
+```
+
+The code replaces the second half (🇬) of the first flag emoji (🇧🇬) and the first half (🇭) of the second flag emoji (🇭🇷), thus changing both the first and second emoji.
---
+++ build.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+const flags = require('@unicode/unicode-14.0.0/Sequence_Property/RGI_Emoji_Flag_Sequence/index.js');
+
+const MAX_LINES_PER_FILE = 10_000;
+
+function* getValidStrings() {
+  for (const flag of flags) {
+    for (const otherFlag of flags) {
+      if (flag === otherFlag) continue;
+      yield `${flag}${otherFlag}`;
+    }
+  }
+}
+
+const hash = (string) => {
+  const codePoints = [...string];
+  const prefix = codePoints[0];
+  const middle = codePoints[1] + codePoints[2];
+  const suffix = codePoints.slice(3).join('');
+  return {
+    id: `${prefix}_${suffix}`,
+    prefix,
+    suffix,
+  };
+};
+
+const mid = (string) => {
+  const codePoints = [...string];
+  const middle = codePoints[1] + codePoints[2];
+  return middle;
+};
+
+// hash(prefix, suffix) => middles
+const map = new Map();
+for (const string of getValidStrings()) {
+  const middle = mid(string);
+  const {id, prefix, suffix} = hash(string);
+  if (map.has(id)) {
+    map.get(id).middles.add(middle);
+  } else {
+    map.set(id, {
+      prefix,
+      suffix,
+      middles: new Set([middle]),
+    });
+  }
+}
+
+const stringify = (string) => {
+  // This is hacky but good enough, since no emoji contains `"`.
+  return JSON.stringify(string).replaceAll('"', '\'');
+};
+
+const gen = ({ prefix, suffix, middleA, middleB }) => {
+  const code = `${
+    stringify(prefix + middleA + suffix)
+  }.replace(${
+    stringify(middleA)
+  }, ${
+    stringify(middleB)
+  }); // → ${stringify(prefix + middleB + suffix)}`;
+  return code;
+};
+
+let fileIndex = 1;
+const writeBuffer = (buffer) => {
+  const output = buffer.join('\n') + '\n';
+  const id = String(fileIndex).padStart(4, '0');
+  const fileName = `./output/part-${id}.js`;
+  console.log(`Writing ${fileName}…`);
+  fs.writeFileSync(fileName, output);
+  fileIndex++;
+};
+
+const buffer = [];
+let lineCount = 0;
+for (const value of map.values()) {
+  const {prefix, suffix, middles} = value;
+  for (const middle of middles) {
+    const middleSymbols = [...middle];
+    for (const otherMiddle of middles) {
+      if (otherMiddle === middle) continue;
+      const otherMiddleSymbols = [...otherMiddle];
+      // Ensure that the first flag changes because of the replacement.
+      if (otherMiddleSymbols[0] === middleSymbols[0]) continue;
+      // Ensure that the second flag changes because of the replacement.
+      if (otherMiddleSymbols[1] === middleSymbols[1]) continue;
+      buffer.push(
+        gen({
+          prefix: prefix,
+          suffix: suffix,
+          middleA: middle,
+          middleB: otherMiddle,
+        })
+      );
+      lineCount++;
+      if (buffer.length === MAX_LINES_PER_FILE) {
+        writeBuffer(buffer);
+        buffer.length = 0;
+      }
+    }
+  }
+}
+
+if (buffer.length > 0) {
+  writeBuffer(buffer);
+}
+
+const nf = new Intl.NumberFormat();
+console.log(`Total number of variations: ${nf.format(lineCount)}`);
